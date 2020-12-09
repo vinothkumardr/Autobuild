2f90b9104c9e47fda9ecab1529a48269a5b462e9
vinothkumardr <63872775+vinothkumardr@users.noreply.github.com>
2020-12-09 21:59:19 +0530

    CalculateSummary
    Calculate programfirst comit

---
+++ Calculate/Calculate/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculate
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Num1 = Convert.ToInt32(textBox1.Text);
+            int Num2 = Convert.ToInt32(textBox2.Text);
+            int result = Num1 + Num2;
+            label1.Text = "Result is " + result.ToString();
+        }
+    }
+}
